13b7ea6749ce66e62d382972cf688d2672f52b0d
陈前 <chenqian@superid.cn>
2020-06-08 22:28:57 +0800

    first commit my github project

---
+++ Leetcode/src/main/java/bianrySearch/BinarySearchTemplate.java
@@ -0,0 +1,80 @@
+package bianrySearch;
+
+import org.junit.Test;
+
+/**
+ * @author Itsuka
+ * @version 1.0
+ * @date 2020/6/8
+ * @description
+ */
+public class BinarySearchTemplate {
+	public int binarySearchOriggin(int[] arr, int target) {
+		if (arr == null) {
+			return -1;
+		}
+		int length = arr.length;
+		if (length == 0) {
+			return -1;
+		}
+
+		int start = 0, end = length - 1;
+
+
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+
+			// 2.先判断是否符合结果
+			if (arr[mid] == target) {
+				return mid;
+			} else if (arr[mid] < target) {
+				// 搜索的区间 [mid+1, end]
+				start = mid + 1;
+			} else {
+
+				end = mid - 1;
+			}
+		}
+		return -1;
+	}
+
+
+	public int binarySearch(int[] arr, int target) {
+		int length = arr.length;
+
+		if (length == 0) {
+			return -1;
+		}
+
+		int start = 0, end = length - 1;
+
+		// 1.严格小于，保证退出循环时条件为 start == end, 也就是只剩下一个元素没有判断
+		while (start < end) {
+			int mid = start + (end - start) / 2;
+			// 肯定有一半是不符合的
+			if (arr[mid] < target) {
+				start = mid + 1;
+			} else {
+				// 和上面最主要的区别，就是把相等和大于归于同一种可能
+				// 每次都排除一半，最后退出的条件是start == end
+				end = mid;
+			}
+		}
+
+		return arr[start] == target ? start : -1;
+	}
+
+	@Test
+	public void test() {
+		int[] arr = {1, 2, 3, 4, 5,7,8};
+
+		System.out.println(binarySearch(arr, 3));
+		System.out.println(binarySearch(arr, 0));
+		System.out.println(binarySearch(arr, 5));
+		System.out.println(binarySearch(arr, -1));
+		System.out.println(binarySearch(arr, 6));
+		System.out.println(binarySearch(arr, 10));
+
+	}
+
+}
---
+++ Leetcode/src/main/java/bianrySearch/第一个大于等于target元素的下标.java
@@ -0,0 +1,48 @@
+package bianrySearch;
+
+/**
+ * @author Itsuka
+ * @version 1.0
+ * @date 2020/6/8
+ * @description 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * <p>
+ * [1,2,3,5,7,10]  target = 8
+ * 5存在，返回5的下标3
+ * <p>
+ * 8不存在，返回要插入的下标地址其实就是10的下标5
+ * 11不存在，返回6
+ */
+public class 第一个大于等于target元素的下标 {
+	/**
+	 * 其实有序查找，就暗示了要二分法，这道题目就是找第一个大于等于target元素的下标，特别的不存在就返回length
+	 *
+	 * @param arr
+	 * @param target
+	 * @return
+	 *[1,2,3,5,6,8] target 7
+	 */
+	public int fisrtBiggerNum(int[] arr, int target) {
+		int length = arr.length;
+
+		if (arr[length - 1] < target) {
+			return length;
+		}
+
+		int start = 0;
+		int end = length;
+		// 退出条件是两者相等
+		while (start < end){
+			int mid = start + (end - start)/2;
+			if (arr[mid] < target){
+				start = mid +1;
+			}else {
+				end = mid;
+			}
+		}
+		// 因为我们每次排除的都是比这个元素小的区间，保留 >= 区间
+		// 最后退出循环就是因为正好是在右边区域，满足题目中条件
+
+
+		return start;
+	}
+}
